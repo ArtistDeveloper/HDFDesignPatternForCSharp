b8b2aad6402cd7b9a3f6dddd13838e2be9b5f25b
Junsu <somadubel@gmail.com>
2022-04-24 02:35:48 +0900

    ADD : Chapter2 ObserverPattern 기본세팅 configuration

---
+++ HDFDesignPatternCSharp/ObserverPattern_WeatherStation/ObserverPattern Implement/WeatherData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ObserverPattern_WeatherStation.ObserverPatternInterface;
+
+namespace ObserverPattern_WeatherStation.ObserverPattern_Implement
+{
+    public class WeatherData : Subject
+    {
+        public void RegisterObserver(Observer o)
+        {
+
+        }
+        public void RemoveObserver(Observer o)
+        {
+
+        }
+        public void NotifyObservers()
+        {
+
+        }
+    }
+}
