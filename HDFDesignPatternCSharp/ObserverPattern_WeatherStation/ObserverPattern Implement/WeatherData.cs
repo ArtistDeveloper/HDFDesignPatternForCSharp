5c7dffc7764ce41be8f49a65789826b58c89d892
Junsu <somadubel@gmail.com>
2022-04-24 13:14:57 +0900

    UPDATE : WeatherData.cs

--- HDFDesignPatternCSharp/ObserverPattern_WeatherStation/ObserverPattern Implement/WeatherData.cs
+++ HDFDesignPatternCSharp/ObserverPattern_WeatherStation/ObserverPattern Implement/WeatherData.cs
@@ -9,14 +9,25 @@ namespace ObserverPattern_WeatherStation.ObserverPattern_Implement
 {
     public class WeatherData : Subject
     {
+        private List<Observer> observers;
+        private float temperature;
+        private float humidity;
+
+        public WeatherData()
+        {
+            observers = new List<Observer>();
+        }
+
         public void RegisterObserver(Observer o)
         {
 
         }
+
         public void RemoveObserver(Observer o)
         {
 
         }
+
         public void NotifyObservers()
         {
 
