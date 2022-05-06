1352c50a5094f7028f1cdbe97858e52f2ceb7b12
Junsu <somadubel@gmail.com>
2022-05-07 03:34:53 +0900

    ADD, UPDATE : StatisDisplay.cs 추가 및 WeatherData 코드추가

---
+++ HDFDesignPatternCSharp/ObserverPattern_WeatherStation/ObserverPattern Implement/MainWeatherStation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern_WeatherStation.ObserverPattern_Implement
+{
+    class MainWeatherStation
+    {
+        static void Main(string[] args)
+        {
+            WeatherData weatherData = new WeatherData();
+
+            CurrentConditionDisplay currenDisplay = new CurrentConditionDisplay(weatherData);
+            StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+
+            weatherData.SetMeasurements(80, 65, 30.4f);
+            weatherData.SetMeasurements(82, 70, 29.2f);
+        }
+    }
+}
